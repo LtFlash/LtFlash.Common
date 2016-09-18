d36c80bc9c620befe5e1ef9e8b408cdfdb581bd1
LtFlash <uVQw8N1Uth@outlook.com>
2016-09-18 16:36:55 +0200

    Improved placing on ground

--- LtFlash.Common/EvidenceLibrary/BaseClasses/EvidenceObject.cs
+++ LtFlash.Common/EvidenceLibrary/BaseClasses/EvidenceObject.cs
@@ -6,14 +6,13 @@ namespace LtFlash.Common.EvidenceLibrary.BaseClasses
     public abstract class EvidenceObject : EvidenceBase
     {
         //PUBLIC
-        public override Vector3 Position
-            { get { return _object ? _object.Position : Vector3.Zero; } }
+        public override Vector3 Position 
+            => _object ? _object.Position : Vector3.Zero;
 
         public override PoolHandle Handle
-            { get { return _object ? _object.Handle : new PoolHandle(); } }
+            => _object ? _object.Handle : new PoolHandle();
 
-        protected override Entity EvidenceEntity
-            { get { return _object; } }
+        protected override Entity EvidenceEntity => _object;
 
         //PROTECTED
         protected Object _object;
@@ -24,15 +23,35 @@ namespace LtFlash.Common.EvidenceLibrary.BaseClasses
         {
             _object = new Object(model, position);
 
-            PlaceObjectOnGround(_object);
+            PlaceOnGround(_object);
 
-            NativeFunction.Natives.SetEntityHasGravity(_object, true);
-            GameFiber.Sleep(3000);
-            _object.IsPositionFrozen = true;
+            ActivateStage(Process_PlaceOnGround); 
+            //PlaceObjectOnGround(_object);
+
+            //NativeFunction.Natives.SetEntityHasGravity(_object, true);
+            //GameFiber.Sleep(3000);
+            //_object.IsPositionFrozen = true;
+        }
+
+        private void Process_PlaceOnGround()
+        {
+            if(Vector3.Distance(PlayerPos, Position) < DistanceEvidenceClose)
+            {
+                PlaceOnGround(_object);
+                DeactivateStage(Process_PlaceOnGround);
+            }
         }
 
         private void PlaceObjectOnGround(Object obj)
             => NativeFunction.Natives.PlaceObjectOnGroundProperly(obj);
+
+        private void PlaceOnGround(Object obj)
+        {
+            NativeFunction.Natives.RequestCollisionAtCoord(obj.Position.X, obj.Position.Y, obj.Position.Z);
+            NativeFunction.Natives.SetEntityDynamic(obj, true);
+            float? po = World.GetGroundZ(obj.Position, false, true);
+            if (po.HasValue) obj.SetPositionZ(po.Value);
+        }
 
         public override void Dismiss()
         {
